bff405f5b71615e6f1f0f70bcf787caa60337d69
Ebraheemugass01 <ibrahimugass01@gmail.com>
2023-03-23 18:27:49 -0700

    wacha tu

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
 #include "main.h"
-
-
 /**
  * main - print _putchar
- * is written by ibrahim 
+ * is written by 
  * Return: 0
  */
-
 int main(void)
-
 {
 _putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
+_putchar('p');
+_putchar('u');
+_putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-
 }
-
